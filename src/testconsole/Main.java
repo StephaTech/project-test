16a0548b8c99817d2fcac8bbb3ecdc990e2bbd93
StephaTech <stephanysouza.direito@gmail.com>
2020-04-30 22:06:42 +0100

    GUI to Console
    small piece of code GUI interface button with actionListener showing at the console.
    For help me to keep doing the UltraVision project and any others.

--- src/testconsole/Main.java
+++ src/testconsole/Main.java
@@ -1,5 +1,13 @@
 package testconsole;
 
+/*The source code below has been sourced     *    SapphireCode on youtube
+Title: <C# vs Java - Console and GUI>
+Author: <SapphireCode>
+Date: <12 July of 2014>
+Code version: <code version>
+Availability: https://www.youtube.com/watch?v=TV9amMrJSfg&t=24s
+*/
+
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -15,13 +23,13 @@ public class Main extends JFrame {
 		
 		setLayout(new FlowLayout());
 		
-		button = new JButton("Output to Console");
+		button = new JButton("Search Titles");
 		add(button);
 		
 		thehandler handler = new thehandler();
 		button.addActionListener(handler);
 		
-		setTitle("Console Output");
+		setTitle("Ultra Vision System");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setLocationRelativeTo(null);
 		setSize(400,400);
@@ -36,7 +44,8 @@ class thehandler implements ActionListener{
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		System.out.println("Hello World");
+		System.out.println("Type Title Name: ");
+		//System.out.println("Women code too");
 		
 	}
 	}
